c680a5afdb36acd71bba7277f58f96005b40ce19
Aaron Jackson <aaron@aaronsplace.co.uk>
2021-08-30 18:40:03 +0100

    Simple / portable config

--- init.el
+++ init.el
@@ -12,14 +12,6 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
-(load-file "~/.emacs.d/website.el")     ;; func to make blog post
-(load-file "~/.emacs.d/mu4e-config.el") ;; email configuration
-
-(setenv "PATH" (concat
-		(getenv "PATH")
-		":/usr/local/texlive/2017/bin/x86_64-linux/"))
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; SETTINGS
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -43,14 +35,10 @@
 (setq-default show-trailing-whitespace t) ;; i hate it
 (global-set-key (kbd "<f12>") 'delete-trailing-whitespace)
 
-(tool-bar-mode -1) ;; gui / style
-(menu-bar-mode -1)
-
 (require 'ido)
 (setq ido-enable-flex-matching t
       ido-everywhere t)
 (ido-mode t)
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; CUSTOM FUNCTIONS
@@ -65,119 +53,10 @@
     (comment-or-uncomment-region beg end)))
 (global-set-key (kbd "C-/") 'asj/comment-or-uncomment-region-or-line)
 
-(defun asj/test ()
-  (interactive)
-  "Test")
-
-
 (defun asj/disable-special ()
   (interactive)
   (setq show-trailing-whitespace nil)
   (linum-mode -1))
-
-(defun asj/set-font-size (n)
-  (interactive "nFont Size: ")
-  (set-face-attribute 'default nil :height (* n 10)))
-
-(asj/set-font-size 10)
-
-;; By Bozhidar Batsov
-(defun google ()
- "Google the selected region if any, display a query prompt otherwise."
- (interactive)
- (browse-url
-  (concat
-   "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
-   (url-hexify-string (if mark-active
-			  (buffer-substring
-			   (region-beginning) (region-end))
-			  (read-string "Search Google: "))))))
-(global-set-key (kbd "C-x g") 'google)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; SPELL CHECKING
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq ispell-program-name "aspell"
-      ispell-extra-args '("--sug-mode=ultra"))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; PLAIN TEXT EDITING
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(add-hook 'text-mode-hook 'auto-fill-mode)
-(add-hook 'text-mode-hook 'flyspell-mode)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; LATEX EDITING
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(check-installed 'auctex)
-(setenv "PATH" "/usr/bin/:$PATH" t)
-
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-(setq reftex-plug-into-AUCTeX t)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Auto complete
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(check-installed 'company)
-
-(setq company-idle-delay 0)
-(global-company-mode)
-
-(check-installed 'company-math)
-(add-hook 'LaTeX-mode-hook
-          (lambda ()
-            (add-to-list 'company-backends 'company-math-symbols-latex)
-            (add-to-list 'company-backends 'company-latex-commands)))
-(check-installed 'company-auctex)
-(company-auctex-init)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; WEB DEVELOPMENT
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(check-installed 'web-mode)
-(add-hook 'web-mode-hook
-          '(lambda ()
-             (setq web-mode-markup-indent-offset 2
-                   web-mode-css-indent-offset 2
-                   web-mode-code-indent-offset 2)))
-(add-to-list 'auto-mode-alist  '("\\.php\\'" . web-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; IRC/ERC
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq erc-server "bnc.irccloud.com"
-      erc-port 6697
-      erc-nick "asjackson"
-      erc-user-full-name "asjackson"
-      erc-prompt-for-password nil
-      erc-autojoin-channels-alist
-      '()
-      erc-save-buffer-on-part nil
-      erc-save-queries-on-quit nil
-      erc-log-write-after-send t
-      erc-log-write-after-insert t)
-
-(add-hook 'erc-mode 'asj/disable-special)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Multiple cursors
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(global-set-key (kbd "C-c m e") 'mc/edit-lines)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; QuickLisp / sbcl
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;(load (expand-file-name "~/quicklisp/slime-helper.el"))
-;(setq inferior-lisp-program "sbcl")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; OpenSCAD
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(load-file "~/.emacs.d/scad-mode.el")
-(load-file "~/.emacs.d/scad-preview.el")
-(require 'scad-mode)
-(require 'scad-preview)
 
 
 
